99dce8f959442f63dc683dd6ec3cbed42e228a45
Nina E <ainina.esham@gmail.com>
2022-06-08 12:10:21 +0800

    Add if statement to ensure valid player choice

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function playerPlay() { // create function for player to choose
         playerSelection === 'scissors') {
         playerSelection = playerSelection; // store valid choice in playerSelection variable
     } else {
-        alert("Fine, then don't play."); // reject player input
+        alert(`Fine, then don't play.`);
     }
     return playerSelection;
 }
@@ -23,30 +23,27 @@ function compareSelection() { // create function to compare selection
     let computerSelection = computerPlay(); // call computerPlay() and assign return value to computerSelection
     let playerSelection = playerPlay(); // call playerPlay() and assign return value to playerSelection
 
-//if (playerSelection === computerSelection) { // check if both selections are the same
-//    console.log(`It's a tie!`); // announce tie
-//} else if ((playerSelection === 'rock' && computerSelection === 'scissors') || // check if player wins against computer
-//            (playerSelection === 'paper' && computerSelection === 'rock') ||
-//            (playerSelection === 'scissors' && computerSelection === 'paper')) {
-//    console.log(`You win! ${playerSelection} beats ${computerSelection}`); // output victory message with playerSelection and computerSelection
-//} else { // otherwise player loses
-//    console.log(`You lose. ${computerSelection} beats ${playerSelection}`); // output defeat message
-//}
-
-    switch(true) { // add switch statement to compare selections
-        case playerSelection === computerSelection: // check if both selections are the same
-            console.log(`It's a tie!`);
-        break;
-        case playerSelection === 'rock' && computerSelection === 'scissors': // check if player wins against computer
-        case playerSelection === 'paper' && computerSelection === 'rock':
-        case playerSelection === 'scissors' && computerSelection === 'paper':
-            console.log(`You win! ${playerSelection} beats ${computerSelection}`); // output victory message with playerSelection and computerSelection
-            playerScore++; // add 1 point to player's score for each player win   
-        break;
-        default: // otherwise player loses
-            console.log(`You lose. ${computerSelection} beats ${playerSelection}`); // output defeat message
-            computerScore++; // add 1 point to computer's score for each computer win
-        break;
+    if ((playerSelection === 'rock') || (playerSelection === 'paper') || (playerSelection === 'scissors')) { // make sure player input is valid choice
+        switch(true) { // add switch statement to compare selections
+            case playerSelection === computerSelection: // check if both selections are the same
+                console.log(`It's a tie!`);
+            break;
+            case playerSelection === 'rock' && computerSelection === 'scissors': // check if player wins against computer
+            case playerSelection === 'paper' && computerSelection === 'rock':
+            case playerSelection === 'scissors' && computerSelection === 'paper':
+                console.log(`You win! ${playerSelection} beats ${computerSelection}`); // output victory message with playerSelection and computerSelection
+                playerScore++; // add 1 point to player's score for each player win   
+            break;
+            case computerSelection === 'scissors' && playerSelection === 'paper':
+            case computerSelection === 'rock' && playerSelection === 'scisssors':
+            case computerSelection === 'paper' && playerSelection === 'rock':
+                console.log(`You lose. ${computerSelection} beats ${playerSelection}`); // output defeat message
+                computerScore++; // add 1 point to computer's score for each computer win
+            break;
+        }
+    } else { // if player choice is invalid then automatic computer win
+        console.log(`Don't know what you chose. Automatic LOSS!`);
+        computerScore++;
     }
 
     console.log(`Your score: ${playerScore}, Computer score: ${computerScore}`); // output message showing player score and computer score each round
