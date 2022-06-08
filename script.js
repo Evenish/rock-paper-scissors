59f19230e202644fe1dff23d6d5830665e38a202
Nina E <ainina.esham@gmail.com>
2022-06-08 12:28:11 +0800

    Remove comment clutter

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function computerPlay() { // create function for computer to choose
-    const choices = ['rock', 'paper', 'scissors']; // create array of choices
-    const computerSelection = choices[Math.floor(Math.random() * choices.length)]; // ask to randomly choose from array
+function computerPlay() { 
+    const choices = ['rock', 'paper', 'scissors']; 
+    const computerSelection = choices[Math.floor(Math.random() * choices.length)]; 
     return computerSelection;
 }
 
-function playerPlay() { // create function for player to choose
-    let playerSelection = prompt("Choose rock, paper, scissors").toLowerCase(); // ask input from player and make case insensitive
+function playerPlay() { 
+    let playerSelection = prompt("Choose rock, paper, scissors").toLowerCase(); // make input case insensitive
     if (playerSelection === 'rock' || // ensure that player puts in valid choice
-        playerSelection === 'paper' ||
-        playerSelection === 'scissors') {
-        playerSelection = playerSelection; // store valid choice in playerSelection variable
+            playerSelection === 'paper' ||
+            playerSelection === 'scissors') {
+        playerSelection = playerSelection; 
     } else {
         alert(`Fine, then don't play.`);
     }
@@ -19,25 +19,27 @@ function playerPlay() { // create function for player to choose
 let playerScore = 0; // create global variable to store player's score
 let computerScore = 0; // create global variable to store computer's score
 
-function compareSelection() { // create function to compare selection
-    let computerSelection = computerPlay(); // call computerPlay() and assign return value to computerSelection
-    let playerSelection = playerPlay(); // call playerPlay() and assign return value to playerSelection
+function compareSelection() { 
+    let computerSelection = computerPlay(); // assign return value to local variable computerSelection
+    let playerSelection = playerPlay(); // assign return value to local variable playerSelection
 
-    if ((playerSelection === 'rock') || (playerSelection === 'paper') || (playerSelection === 'scissors')) { // make sure player input is valid choice
-        switch(true) { // add switch statement to compare selections
-            case playerSelection === computerSelection: // check if both selections are the same
+    if ((playerSelection === 'rock') ||
+        (playerSelection === 'paper') ||
+        (playerSelection === 'scissors')) { // make sure player input is valid choice
+        switch(true) { 
+            case playerSelection === computerSelection: 
                 console.log(`It's a tie!`);
             break;
-            case playerSelection === 'rock' && computerSelection === 'scissors': // check if player wins against computer
+            case playerSelection === 'rock' && computerSelection === 'scissors': 
             case playerSelection === 'paper' && computerSelection === 'rock':
             case playerSelection === 'scissors' && computerSelection === 'paper':
-                console.log(`You win! ${playerSelection} beats ${computerSelection}`); // output victory message with playerSelection and computerSelection
+                console.log(`You win! ${playerSelection} beats ${computerSelection}`); 
                 playerScore++; // add 1 point to player's score for each player win   
             break;
             case computerSelection === 'scissors' && playerSelection === 'paper':
             case computerSelection === 'rock' && playerSelection === 'scisssors':
             case computerSelection === 'paper' && playerSelection === 'rock':
-                console.log(`You lose. ${computerSelection} beats ${playerSelection}`); // output defeat message
+                console.log(`You lose. ${computerSelection} beats ${playerSelection}`); 
                 computerScore++; // add 1 point to computer's score for each computer win
             break;
         }
@@ -46,7 +48,7 @@ function compareSelection() { // create function to compare selection
         computerScore++;
     }
 
-    console.log(`Your score: ${playerScore}, Computer score: ${computerScore}`); // output message showing player score and computer score each round
+    console.log(`Your score: ${playerScore}, Computer score: ${computerScore}`); 
 }
 
 function game() { // create function that plays for 5 rounds
